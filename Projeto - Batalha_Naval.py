99145ec553815f4b3b2791ce9a07723d2c5bd4f2
Pedro Medeiros das Chagas <111517334+Pedropschagas@users.noreply.github.com>
2022-12-21 00:10:51 -0300

    Frontend ok
    Adicionado todo o Frontend, falta apenas verificar uma parte da lógica do jogo do pc.

--- Projeto - Batalha_Naval.py
+++ Projeto - Batalha_Naval.py
@@ -1,20 +1,28 @@
-
+import os
 import random # importando a biblioteca para randomizar números
+import time
 
 titulo = ''
 tabuleiroPc = []
 tabuleiroJogador = []
-tamanho_tabuleiro = 5  # entrada do tamanho do tabuleiro.
+tamanho_tabuleiro = 0  # entrada do tamanho do tabuleiro.
 caractereBarco = '<'
 caractereRedor = '-'
+caractereAgua = '*'
 barco = [0, '']
 celula = [0, 0]
-tamanhoDosBarcos = [5, 5, 4, 4, 3, 3, 2, 2]
+tamanhoDosBarcos = [5, 5, 4, 4, 3, 2, 2]
+tamanhoMaxBarco = []
 placar = [0, 0]
 flag = 0
 atingiuBarco = False
 tentativa = []
 sentidoTiroBarco = 'direita'
+pontosGanhador = 0
+
+
+def limparTela():
+    os.system('cls')
 
 # Laço construtor de tabuleiro.
 def construirTabuleiro(tamanho_tabuleiro, tabuleiro):
@@ -24,8 +32,17 @@ def construirTabuleiro(tamanho_tabuleiro, tabuleiro):
             lista.append('*')
         tabuleiro.append(lista)
 
-
+# função para mostrar tabuleiro na tela
 def imprimirTabuleiro():
+    limparTela()
+    
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|                         BATALHA NAVAL                          |")
+    print("|                                                                |")  
+    print(f'                   PLACAR ==> JOGADOR {placar[0]} X PC {placar[1]}')  
+    print("")
+    print("")
     limite = tamanho_tabuleiro
     if limite > 10:
         limite = 10
@@ -52,13 +69,45 @@ def imprimirTabuleiro():
     for i in range(tamanho_tabuleiro):
         print(referenciaLetra[i], end='  ')
         for j in range(tamanho_tabuleiro):
-            print(tabuleiroJogador[i][j], end='  ')
+            if tabuleiroJogador[i][j] == caractereRedor:
+                print(caractereAgua, end='  ')
+            else:
+                print(tabuleiroJogador[i][j], end='  ')
         print('   ||     ', referenciaLetra[i], end='  ')
         for j in range(tamanho_tabuleiro):
-            print(tabuleiroPc[i][j], end='  ')
+            if tabuleiroPc[i][j] == caractereRedor or tabuleiroPc[i][j] == caractereBarco:
+                print(caractereAgua, end='  ')
+            else:
+                print(tabuleiroPc[i][j], end='  ')
         print()
-    print(f'PLACAR\n JOGADOR {placar[0]} X PC {placar[1]}')
+    print()
 
+# funcção para mostrar apenas o tabuleiro do jogador no início do jogo
+def imprimirTabuleiroJogador():
+    global tamanho_tabuleiro
+    limite = tamanho_tabuleiro
+    if limite > 10:
+        limite = 10
+    referenciaLetra = ['A', 'B', 'C', 'D', 'E', 'F',
+                       'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
+    print('   ', end='')
+    for i in range(1, limite):
+        print(i, end='  ')
+    print(limite, end=' ')
+    if tamanho_tabuleiro > 10:
+        for i in range(11, tamanho_tabuleiro + 1):
+            print(i, end=' ')
+            
+    print()
+
+    for i in range(tamanho_tabuleiro):
+        print(referenciaLetra[i], end='  ')
+        for j in range(tamanho_tabuleiro):
+            if tabuleiroJogador[i][j] == caractereRedor:
+                print(caractereAgua, end='  ')
+            else:
+                print(tabuleiroJogador[i][j], end='  ')
+        print()
 
 # A função verifica se o barco estoura o espaço do tabuleiro e se fica muito próximo de outro barco já posicionado
 def verificarSePodePosicionar(tabuleiro, celula, barco):
@@ -76,12 +125,10 @@ def verificarSePodePosicionar(tabuleiro, celula, barco):
                 return False
     return True
 
-
 # Função para posicionar o barco num tabuleiro
 def posicionarBarco(tabuleiro, celula, barco):
 
     if not verificarSePodePosicionar(tabuleiro, celula, barco):
-        # print("Você não pode colocar barco nessa posição!")
         return False
 
     if barco[1] == 'horizontal':
@@ -106,7 +153,6 @@ def marcarRedorBarco(tabuleiro):
                             if tabuleiro[i-1+k][j-1+l] == caractereBarco:
                                 tabuleiro[i][j] = caractereRedor
 
-
 # Função para posicionar um barco de forma randomica, se um barco não puder ser posicionado ela vai tentar 100 vezes.
 def posicionarBarcoCPU(tabuleiro, tamanhoBarco):
     maximoTentativas = 0
@@ -128,10 +174,22 @@ def posicionarBarcoCPU(tabuleiro, tamanhoBarco):
         maximoTentativas += 1
     return False
 
-
 # Aqui a função que vai analisar quantos barcos deve inserir automaticamente para a CPU.
 def CPUBarcos():
-    tamanhoMaxBarco = []
+    global pontosGanhador
+    global tamanhoMaxBarco
+
+    for i in tamanhoMaxBarco:
+        pontosGanhador += i
+        if not (posicionarBarcoCPU(tabuleiroPc, i)):
+            print(
+                "Número máximo de tentativas para a CPU posicionar barcos estourou, rode o APP novamente!")
+
+# Função que cria uma lista de barcos dependendo do tamanho do tabuleiro escolhido
+def tamanhoBarcosEscolhidos ():
+    global tamanhoDosBarcos
+    global tamanhoMaxBarco
+    global tamanho_tabuleiro
 
     for i in range(len(tamanhoDosBarcos)):
         if tamanho_tabuleiro > 12:
@@ -141,12 +199,7 @@ def CPUBarcos():
         elif tamanhoDosBarcos[i] <= 3:
             tamanhoMaxBarco.append(tamanhoDosBarcos[i])
 
-    for i in tamanhoMaxBarco:
-        if not (posicionarBarcoCPU(tabuleiroPc, i)):
-            print(
-                "Número máximo de tentativas para a CPU posicionar barcos estourou, rode o APP novamente!")
-
-#Função para ordenar ataques e placar.
+# Função para ordenar ataques e placar.
 def tiro(tabuleiro, celula, atirador):
     if tabuleiro[celula[0]][celula[1]] == caractereBarco:
         tabuleiro[celula[0]][celula[1]] = 'X'
@@ -157,13 +210,24 @@ def tiro(tabuleiro, celula, atirador):
         imprimirTabuleiro()
         return True
     else:
+        if tabuleiro[celula[0]][celula[1]] == 'X' or tabuleiro[celula[0]][celula[1]] == '0':
+            imprimirTabuleiro()
+            return False
         tabuleiro[celula[0]][celula[1]] = '0'
         imprimirTabuleiro()
         return False
 
+# Função para verificar o vitorioso
+def vitoria():
+    global pontosGanhador
+    global placar
+    if placar[0] == pontosGanhador:
+        return 1
+    elif placar[1] == pontosGanhador:
+        return 2
+    return 0
 
-#Função para ordenar ataque do PC.
-
+# Função para ordenar ataque do PC.
 def logicaPc():
     global flag
     global sentidoTiroBarco
@@ -299,61 +363,235 @@ def logicaPc():
                         flag = 0
                         return
 
+# Função para jogar
+def jogando():
+    while vitoria() == 0:
+        print("Escolha uma coordenada para atacar")
 
-# Main
-construirTabuleiro(tamanho_tabuleiro, tabuleiroJogador)
-construirTabuleiro(tamanho_tabuleiro, tabuleiroPc)
+        celula= [0,0]
+        entrada = input("Escolha a Linha:").lower()
+        try:
+            entrada = ord(entrada)-97
+        except:
+            entrada = -1
+        while entrada >= tamanho_tabuleiro or entrada < 0:
+            imprimirTabuleiro()
+            print("Escolha uma linha válida!")
+            entrada = input("Escolha a Linha:").lower()
+            try:
+                entrada = ord(entrada)-97
+            except:
+                entrada = -1
+        celula[0] = entrada
 
-barco = [3, 'horizontal']
-celula = [2, 2]
-posicionarBarco(tabuleiroJogador, celula, barco)
+        try:
+            entrada = int(input("Escolha a Coluna:"))
+        except:
+            entrada = -1
+        entrada -= 1
+        while entrada >= tamanho_tabuleiro or entrada < 0:
+            imprimirTabuleiro()
+            print("Escolha uma coluna válida!")
+            try:
+                entrada = int(input("Escolha a Coluna:"))
+            except:
+                entrada = -1      
+            entrada -= 1
+        
+        celula[1] = entrada
 
-barco = [5, 'vertical']
-celula = [8, 5]
-posicionarBarco(tabuleiroJogador, celula, barco)
+        if not tiro(tabuleiroPc, celula, "jogador"):
+            logicaPc()
+        imprimirTabuleiro()
+    if vitoria() == 1:
+        telaDaVitoriaJogador()
+    else:
+        telaDaVitoriaPc()
 
-barco = [2, 'horizontal']
-celula = [1, 13]
-posicionarBarco(tabuleiroJogador, celula, barco)
+# !!!!!!!!!!!!!!!!!!!!        TELAS DE EXIBIÇÃO        !!!!!!!!!!!!!!!!!
+def telaApersentacao():
+    limparTela()
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|                                                                |")    
+    print("|                       JOGO BATALHA NAVAL                       |")
+    print("|                                                                |")    
+    print("|                                                                |")    
+    print("------------------------------------------------------------------")
+    print("                           INSTRUÇÕES:")
+    print("Legenda de símbolos e suas representações:")
+    print(caractereAgua, " = água")
+    print(caractereBarco, " = barco")
+    print("X = barco atingido")
+    print("0 = tiro na água")
+    print("------------------------------------------------------------------")
+    print("Pressione ENTER para inciar")
 
-barco = [4, 'vertical']
-celula = [11, 12]
-posicionarBarco(tabuleiroJogador, celula, barco)
+def telaEscolhaTamanhoTabuleiro():
+    limparTela()
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|                 ESCOLHA A DIFICULDADE DO JOGO                  |")
+    print("|                                                                |")    
+    print("------------------------------------------------------------------")
+    print("Digite:")
+    print("F = Fácil")
+    print("M = Moderado")
+    print("D = Difícil")
 
-barco = [2, 'vertical']
-celula = [11, 0]
-posicionarBarco(tabuleiroJogador, celula, barco)
+def telaEscolherBarcos(tamanho):
+    limparTela()
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|               POSICIONE SEUS BARCOS NO TABULEIRO               |")
+    print("|                                                                |")    
+    print("------------------------------------------------------------------")
+    imprimirTabuleiroJogador()
+    print("Você vai escolher a orientação do seu navio de tamanho", tamanho, "agora.")
+    print("O navio, quando na horizontal, será posicionado da esquerda para direita.")
+    print("O navio, quando na vertical, será posicionado de cima para baixo.")
+    print("Não poderão haver Navios adjacentes.")
 
-barco = [3, 'horizontal']
-celula = [2, 2]
-posicionarBarco(tabuleiroJogador, celula, barco)
+def telaNaviosJogadorPosicionados():
+    limparTela()
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|                                                                |")    
+    print("|                     NAVIOS POSICIONADOS!                       |")
+    print("|                  PREPARE-SE PARA A BATALHA!                    |")    
+    print("|                                                                |")     
+    print("|                                                                |")    
+    print("------------------------------------------------------------------")
+    imprimirTabuleiroJogador()
 
-CPUBarcos()
+def telaContagemNaviosPC():
+    for i in range(5,-1,-1):
+        limparTela()
+        print("------------------------------------------------------------------")
+        print("|                                                                |")
+        print("|                                                                |")    
+        print("|                COMPUTADOR POSICIONANDO NAVIOS                  |")
+        print("|                                                                |")            
+        print("|                             ",i,"                                |")    
+        print("|                                                                |")     
+        print("|                                                                |")    
+        print("------------------------------------------------------------------")
+        time.sleep(1)
 
-posicaoTiro = [2, 2]
-tiro(tabuleiroPc, posicaoTiro, 'jogador')
-logicaPc()
+def telaDaVitoriaJogador():
+    limparTela()
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|                                                                |")    
+    print("|                            PARABÉNS!                           |")
+    print("|                    VOCÊ DERROTOU O INIMIGO!                    |")    
+    print("|                                                                |")     
+    print("|                                                                |")    
+    print("------------------------------------------------------------------")
 
-posicaoTiro = [1, 1]
-tiro(tabuleiroPc, posicaoTiro, 'jogador')
-logicaPc()
+def telaDaVitoriaPc():
+    limparTela()
+    print("------------------------------------------------------------------")
+    print("|                                                                |")
+    print("|                                                                |")    
+    print("|                            PARABÉNS!                           |")
+    print("|                          VOCÊ  PERDEU!                         |")    
+    print("|                                                                |")     
+    print("|                                                                |")    
+    print("------------------------------------------------------------------")
 
-posicaoTiro = [3, 3]
-tiro(tabuleiroPc, posicaoTiro, 'jogador')
-logicaPc()
-
-imprimirTabuleiro()
+def startGame():    
+    barco = [0, '']
+    celula = [0,0]  
 
 
-# print()
-# print()
+    global tamanho_tabuleiro
+    entrada = ""
+    telaApersentacao()
+    entrada = input().lower()
+    print(entrada)
+    if entrada != '€³¤€üé®þü³¤€¼üé®þ':
+        telaEscolhaTamanhoTabuleiro() 
+        entrada = input().lower()
+        while True:
+            if entrada == 'f':
+                tamanho_tabuleiro = 5
+                break
+            elif entrada == 'm':
+                tamanho_tabuleiro = 10
+                break
+            elif entrada == 'd':
+                tamanho_tabuleiro = 15
+                break
+            else:
+                print("Opção Inválida!")
+                print("Digite uma opção correta!")                
+                entrada = input().lower()
+        construirTabuleiro(tamanho_tabuleiro, tabuleiroJogador)
+        construirTabuleiro(tamanho_tabuleiro, tabuleiroPc)
+        tamanhoBarcosEscolhidos()
 
-# print(' '*54, 'BATALHA NAVAL')
-# print('-_'*60)
-# print()
-# jogador1 = input('Diga-me, Jogador1, qual é o seu nome?  ')
-# print()
-# print()
-# print()
-# print()
-# print(' '*28, f'Olá, {jogador1}, POSICIONE SUAS EMBARCAÇÕES')
+
+        for i in tamanhoMaxBarco:                 
+            telaEscolherBarcos(i)
+            barco[0] = i       
+            while True:                
+                while True:                
+                    entrada = input("Escolha o sentido (H = horizontal | V = Vertical):").lower()
+                    if entrada == 'h':
+                        barco[1] = "horizontal"
+                        break
+                    if entrada == 'v':
+                        barco[1] = "vertical"
+                        break
+                    else:
+                        telaEscolherBarcos(i)
+                        print("Opção Inválida! Escolha um sentido válido.")
+
+
+                entrada = input("Escolha a Linha:").lower()
+                try:
+                    entrada = ord(entrada)-97
+                except:
+                    entrada = -1
+                while entrada >= tamanho_tabuleiro or entrada < 0:
+                    telaEscolherBarcos(i)
+                    print("Escolha uma linha válida!")
+                    entrada = input("Escolha a Linha:").lower()
+                    try:
+                        entrada = ord(entrada)-97
+                    except:
+                        entrada = -1
+                celula[0] = entrada
+
+                try:
+                    entrada = int(input("Escolha a Coluna:"))
+                except:
+                    entrada = -1
+                entrada -= 1
+                while entrada >= tamanho_tabuleiro or entrada < 0:
+                    telaEscolherBarcos(i)
+                    print("Escolha uma coluna válida!")
+                    try:
+                        entrada = int(input("Escolha a Coluna:"))
+                    except:
+                        entrada = -1      
+                    entrada -= 1
+                
+                celula[1] = entrada
+
+                if posicionarBarco(tabuleiroJogador, celula, barco):
+                    break                                  
+                telaEscolherBarcos(i)
+                print("Posição inválida! Posicione novamente.")
+        
+        telaNaviosJogadorPosicionados()
+
+    time.sleep(5)    
+    telaContagemNaviosPC()           
+    CPUBarcos()
+    imprimirTabuleiro()
+
+    jogando()
+
+startGame()
